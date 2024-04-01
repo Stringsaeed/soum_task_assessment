ed8e9ec6342f50b35850900debe09550cee7e70d
Muhammed Saeed <stringsaeed@gmail.com>
2024-04-01 23:08:05 +0400

    test: update test configuration for typescript paths

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,30 @@
+const { compilerOptions } = require('./tsconfig.json');
+
 const packagesToTransform = [
-  "react-native",
-  "react-native-(.*)",
-  "@react-native/(.*)",
-  "@react-native-community",
-  "@react-navigation",
-  "expo",
-  "expo-(.*)",
-  "@expo(nent)?",
-  "@expo-google-fonts",
-  "react-navigation",
-  "@react-navigation/.*",
-  "@unimodules",
-  "unimodules",
+  'react-native',
+  'react-native-(.*)',
+  '@react-native/(.*)',
+  '@react-native-community',
+  '@react-navigation',
+  'expo',
+  'expo-(.*)',
+  '@expo(nent)?',
+  '@expo-google-fonts',
+  'react-navigation',
+  '@react-navigation/.*',
+  '@unimodules',
+  'unimodules',
 ];
 
 /** @type {import("jest").Config} */
 module.exports = {
-  preset: "react-native",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  transformIgnorePatterns: [
-    `node_modules/(?!(${packagesToTransform.join("|")})/)`,
-  ],
+  preset: 'react-native',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transformIgnorePatterns: [`node_modules/(?!(${packagesToTransform.join('|')})/)`],
+  testRegex: '\\.test\\.[jt]sx?$',
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
